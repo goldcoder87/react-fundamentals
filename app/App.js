18bfde62b156767615fc0a4aac78c2758885fc32
Solomon <sdgoldiamond@gmail.com>
2017-05-26 08:50:45 -0400

    first commit

---
+++ app/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import data from './data';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default App;
